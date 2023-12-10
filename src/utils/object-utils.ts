2b555b1baa12beadd86d1f6fb220f7b26cb7fa10
cjeanm11 <77751904+cjeanm11@users.noreply.github.com>
2023-12-09 22:21:25 -0500

    first exemple.

--- src/utils/object-utils.ts
+++
@@ -1,7 +0,0 @@
-
-
-export function createObjects<T>(numObjects: number, constructor: new (...args: any[]) => T, args: any[] = []): T[] {
-    return Array.from({ length: numObjects }, (_, index) => {
-        return new constructor(...args); // Use spread operator to pass arguments
-    });
-}
