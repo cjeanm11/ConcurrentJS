a9229edb2d519c20ec6e819dc4f2f16f2768088b
cjeanm11 <77751904+cjeanm11@users.noreply.github.com>
2023-12-09 19:57:10 -0500

    first exemple.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import {ComputationWorkerImpl} from "./computation-worker/computation-worker-impl";
 import {ComputationWorker} from "./computation-worker/computation-worker";
-import {createObjects} from "../utils/object-util";
+import {createObjects} from "./utils/object-util";
 
 // Example usage
 const main = async () : Promise<void> => {
