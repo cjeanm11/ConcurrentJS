43ec9f57475a9f5266c27983e9268706ef5d7a6a
cjeanm11 <77751904+cjeanm11@users.noreply.github.com>
2023-12-09 19:57:10 -0500

    first exemple.

--- src/computationWorker/computation-worker.ts
+++
@@ -1,7 +0,0 @@
-export interface ComputationWorker {
-    execute: (funcName: string, args: any[]) => Promise<any>;
-    onError: (callback: (error: Error) => void) => void;
-    onSuccess: (callback: (result: any) => void) => void;
-    terminate: () => void;
-}
-
